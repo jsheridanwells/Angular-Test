60d1ec160be143f9bcbf14d9a0c39540d3e850ba
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-02 14:03:07 -0500

    email login working

---
+++ app/factories/factory-user.js
@@ -0,0 +1,43 @@
+'use strict';
+
+app.factory('userFactory', function ($q, $http) {
+	let currentUser = null;
+
+	const isAuthenticated = function() {
+		return new Promise((resolve, reject) => {
+			firebase.auth().onAuthStateChange((user) => {
+				if (user) {
+					currentUser = user.uid;
+					resolve(true);
+				} else {
+					resolve(false);
+				}
+			});
+		});
+	};
+
+	const getCurrentUser = function() {
+		return currentUser;
+	};
+
+	const logIn = function(userObj) {
+		return firebase.auth().signInWithEmailAndPassword(userObj.email, userObj.password)
+		.catch((error) => {
+			console.log("error from login", error.code, error.message);
+		});
+	};
+
+	const logOut = function() {
+		firebase.auth().signOut();
+	};
+
+	const register = function(userObj) {
+		return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password)
+		.catch((error)=>{
+			console.log("error in register", error.code, error.message);
+		});
+	};
+
+	return {isAuthenticated, getCurrentUser, logIn, logOut, register};
+
+});
