daf0eeb351670fdffa5ed41394220c6976abe553
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-03 12:18:54 -0500

    delete data functions created

--- app/controllers/controller-data.js
+++ app/controllers/controller-data.js
@@ -1,7 +1,11 @@
 'use strict';
 
-app.controller('dataCtrl', function($scope, dataFactory){
+app.controller('dataCtrl', function($scope, dataFactory, $location, $route){
 	$scope.myData = {};
+	$scope.newItem = {
+		name: '',
+		description: ''
+	};
 
 	const showData = function() {
 		console.log("showData is firing");
@@ -11,5 +15,16 @@ app.controller('dataCtrl', function($scope, dataFactory){
 			console.log("$scope.myData", $scope.myData);
 		});
 	};
+
+	$scope.addItem = function() {
+		dataFactory.addItem($scope.newItem);
+		showData();
+		// $route.reload();
+	};
+
+	$scope.deleteItem = function(item) {
+		dataFactory.deleteItem(item);
+	};
+
 	showData();
 });
